236534d6825293c14b9655d7114429f3fecbe31c
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-06-10 15:16:58 +0200

    feat: creating card

--- components/Main.jsx
+++ components/Main.jsx
@@ -20,12 +20,21 @@ function Main () {
     return (
         <body>
             <div className="container">
-                <ul>
-                    {actress.map((curActress, index) => (
-                        <li key={index} className='list-actress'>{curActress.name}</li>
-                    )
-                )}
-                </ul>
+                    <ul className='boxCard'>
+                        {actress.map((curActress, index) => (
+                            <li key={index} className='list-actress'>
+                                <div className='card'>
+                                    <h3>{curActress.name}</h3>
+                                    <img src={curActress.image} alt="actress-image" />
+                                    <p className='biography'>{curActress.biography}</p>
+                                    <p className='birthyear'>{curActress.birth_year}</p>
+                                    <p className='nationality'>{curActress.nationality}</p>
+                                    <p className='awards'>{curActress.awards}</p>
+                                </div>
+                            </li>
+                        )
+                    )}
+                    </ul>
             </div>
         </body>
     )
